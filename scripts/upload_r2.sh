ebff3962c14887bfb2413e99aab6e4f64688da43
Baptiste Augrain <daiyam@zokugun.org>
2025-04-02 01:48:47 +0200

    fix: censure email address

--- scripts/upload_r2.sh
+++ scripts/upload_r2.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 mkdir -p r2
 
 find pkgs -name '*.deb' -exec bash -c 'mv "$0" "r2/$( basename "$0" )"' {} \;
 find pkgs -name '*.rpm' -exec bash -c 'mv "$0" "r2/$( basename "$0" )"' {} \;
 
-# find r2 -type f -exec bash -c 'npx wrangler r2 object put --remote --file "$0" "${R2_BUCKET_NAME}/$( basename "$0" )" || exit 1' {} \;
 for FILE in r2/*; do
   if [[ -f "${FILE}" ]]; then
-    npx wrangler r2 object put --remote --file "${FILE}" "${R2_BUCKET_NAME}/$(basename "${FILE}")"
+    npx wrangler r2 object put --remote --file "${FILE}" "${R2_BUCKET_NAME}/$(basename "${FILE}")" | sed -E 's/[[:alnum:]._%+-]+@[[:alnum:].-]+\.[[:alpha:]]{2,}/****/g'
   fi
 done
