227161bdaaeb134b0a3d63a03286784b5aab4083
Baptiste Augrain <daiyam@zokugun.org>
2025-04-02 01:18:35 +0200

    wip: upload packages

--- scripts/upload_r2.sh
+++ scripts/upload_r2.sh
@@ -7,4 +7,4 @@ mkdir -p r2
 find pkgs -name '*.deb' -exec bash -c 'mv "$0" "r2/$( basename "$0" )"' {} \;
 find pkgs -name '*.rpm' -exec bash -c 'mv "$0" "r2/$( basename "$0" )"' {} \;
 
-find r2 -type f -exec bash -c 'npx wrangler r2 object put --remote --file "$0" "${R2_BUCKET_NAME}/$( basename "$0" )"' {} \;
+find r2 -type f -exec bash -c 'npx wrangler r2 object put --remote --file "$0" "${R2_BUCKET_NAME}/$( basename "$0" )" || exit 1' {} \;
