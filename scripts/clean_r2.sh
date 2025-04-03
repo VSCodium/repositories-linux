e2604e300e6f30b8cbf8c86fd4b4f663113a1f4c
Baptiste Augrain <daiyam@zokugun.org>
2025-04-03 19:16:17 +0200

    wip: print delete commands

--- scripts/clean_r2.sh
+++ scripts/clean_r2.sh
@@ -4,3 +4,7 @@ set -e
 
 echo "ALL_FILES: ${ALL_FILES}"
 echo "OLD_FILES: ${OLD_FILES}"
+
+for FILE in ${OLD_FILES}; do
+  echo "npx wrangler r2 object delete --remote --file ${FILE}"
+done
--- scripts/upload_r2.sh
+++ scripts/upload_r2.sh
@@ -7,7 +7,7 @@ mkdir -p r2
 find pkgs -name '*.deb' -exec bash -c 'mv "$0" "r2/$( basename "$0" )"' {} \;
 find pkgs -name '*.rpm' -exec bash -c 'mv "$0" "r2/$( basename "$0" )"' {} \;
 
-ALL_FILES=$( npx wrangler kv key get --remote --binding="KV_REPO" "ALL_FILES" )
+ALL_FILES=$( npx wrangler kv key get --remote --namespace-id="${CLOUDFLARE_KV_NAMESPACE_ID}" "ALL_FILES" )
 OLD_FILES="${ALL_FILES}"
 
 for FILE in r2/*; do
@@ -24,7 +24,7 @@ for FILE in r2/*; do
   fi
 done
 
-npx wrangler kv key put --remote --binding="KV_REPO" "ALL_FILES" "${ALL_FILES}"
+npx wrangler kv key put --remote --namespace-id="${CLOUDFLARE_KV_NAMESPACE_ID}" "ALL_FILES" "${ALL_FILES}"
 
 echo "ALL_FILES=${ALL_FILES}" >> "${GITHUB_ENV}"
 echo "OLD_FILES=${OLD_FILES}" >> "${GITHUB_ENV}"
